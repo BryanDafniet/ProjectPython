81cd570f73e18e06e6facfa06596545df42fbdc8
m2isdd LOIC DREANO <ldreano@lk-521a-wle.ens.sdv.univ-paris-diderot.fr>
2017-09-12 16:31:38 +0200

    première création de classe

--- Classe_Atome.py
+++ Classe_Atome.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
-class Acide_Amine(object):
-	def __init__(self):
-		
+class Residus(object):
+	def __init__(self,resid_name = "ASP",resid_num = 0):
+		'''Description '''
+		self.resid_name = resid_name
+		self.num = resid_num
+
+class Atoms(Residus):
+	def __init__(self,resid_name = "ASP",resid_num = 0,atom_name = "CA" ,coord_X = 0.1 ,coord_Y = 0.2, coord_Z = 0.3):
+ 		Residus.__init__(self,resid_name,resid_num)
+ 		self.X = coord_X
+ 		self.Y = coord_Y
+ 		self.Z = coord_Z
+ 		self.atom_name = atom_name
+
+test=Atoms()
+print (test.resid_name,test.atom_name)
